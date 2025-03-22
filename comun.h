0d13cc3d4a40fc10fc5a6ec7a04b3ef11d56adbd
GonzalodeLorenzo <gondelorenzo1@gmail.com>
2025-03-22 18:37:43 +0000

    Añadido comun.c y comun.h

---
+++ comun.h
@@ -0,0 +1,25 @@
+#ifndef COMUN_H
+#define COMUN_H
+
+typedef struct Config
+{
+    int limite_retiro;
+    int limite_transferencia;
+    int umbral_retiros;
+    int umbral_transferencias;
+    int num_hilos;
+    char archivo_cuentas[50];
+    char archivo_log[50];
+} Config;
+
+typedef struct Cuenta
+{
+    int numero_cuenta;
+    char titular[50];
+    float saldo;
+    int num_transacciones;
+} Cuenta;
+
+Config leer_configuracion(const char *ruta);
+
+#endif
