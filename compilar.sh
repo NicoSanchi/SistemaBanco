6d5f99b6c2a2dce18c4d7a7e7ca355d038dc8ff6
GonzalodeLorenzo <gondelorenzo1@gmail.com>
2025-05-16 09:43:33 +0000

    cambios compilador

--- compilar.sh
+++ compilar.sh
@@ -1,3 +1,3 @@
-gcc monitor.c comun.c -o monitor
-gcc banco.c comun.c -o banco
-./banco
+gcc monitor.c comun.c -o monitor -pthread
+gcc banco.c comun.c -o banco -pthread
+gcc usuario.c comun.c -o usuario -pthread
